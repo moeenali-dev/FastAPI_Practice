f12628d68fd40cc0782988f9b7a61a16e63a178d
moeenali-dev <moeenali673@gmail.com>
2026-07-20 14:31:22 +0500

    FastAPI setup with Postman endpoints and created GET request

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi import Body
+
+app = FastAPI()
+
+@app.get("/")
+
+def home():
+    return("Hello World")
+
+@app.get("/posts")
+def root():
+    return("Hi this is my post")
+
+@app.post("/createposts")
+def create_posts(worldtimes : dict = Body):
+  print(worldtimes)
+  return f"Your post is successfully created: \n Title: {worldtimes['title']} Content: {worldtimes['content']}"
