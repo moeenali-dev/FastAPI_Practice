534c6bd2ec9b82912602f4c23cf7851589fd6b2c
moeenali-dev <moeenali673@gmail.com>
2026-07-21 17:06:31 +0500

    Add The HTTP Exception, Validation and Solved the Error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , status , HTTPException
 from fastapi import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -14,7 +14,7 @@ class Post(BaseModel):
     ratings: Optional[int] = None
 
 
-my_posts = [{"title: ": "Hey the title of my post is Foods " , "content:" : "Pakistani food is very scrumptious" , "published: ": "Yes it is published" , "id" : 1} , {"title: ": "Hey the title of my post is Foods " , "content:" : "Mexican food is very scrumptious" , "published: ": "Yes it is published" , "id" : 2}]
+my_posts = [{"title": "Hey the title of my post is Foods " , "content" : "Pakistani food is very scrumptious" , "published": "Yes it is published" , "id" : 1} , {"title": "Hey the title of my post is Foods " , "content" : "Mexican food is very scrumptious" , "published": "Yes it is published" , "id" : 2}]
 
     
 @app.get("/")
@@ -30,7 +30,6 @@ def create_posts(posts: Post):
     post_dict = posts.model_dump()
     post_dict ['id'] = randrange(0,1000)
     my_posts.append(post_dict)
-    # return f"Your post is successfully created: \n Title:{posts.title} \n Content: {posts.content} \n Published: {posts.published} \n Reviews: {posts.reviews}"
     return {"data: " : post_dict}
 
 
@@ -38,21 +37,27 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p 
+    return None
     
 
 @app.get("/posts/{id}")
-def get_specific_post(id: int):
-    post = find_post(int(id))
+def get_specific_post(id: int ):
+    post = find_post(int(id) )
+    if post is None:
+       raise HTTPException (status_code=status.HTTP_404_NOT_FOUND , detail=f"Your post of id {id} is not found")
     return {"Posts Details: Here is your post: ": post}
 
 def get_index(id):
     for i, p in enumerate (my_posts):
         if p["id"] == id:
             return i
+    return None
         
 @app.delete("/posts/{id}")
 def delete_post(id: int):
     post = get_index(id)
+    if post is None:
+        raise HTTPException (status_code=status.HTTP_404_NOT_FOUND , detail=f"Your post of id {id} is not found")
     my_posts.pop(post)
-    return {f"Your post": "{id} has been deleted successfully"}
+    return {"message": "Your post" f"{id} has been deleted successfully"}
     
