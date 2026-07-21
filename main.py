fb768fc56792cea88e03e3b261f9567e410bc328
moeenali-dev <moeenali673@gmail.com>
2026-07-21 15:30:46 +0500

    Added to Find the Specific Post

--- main.py
+++ main.py
@@ -23,11 +23,25 @@ def home():
 
 @app.get("/posts")
 def root():
-    return("Hi this is my post")
+    return{"data: ":  my_posts}
 
 @app.post("/posts") 
 def create_posts(posts: Post): 
-    post_dict = posts.model.dump()
+    post_dict = posts.model_dump()
     post_dict ['id'] = randrange(0,1000)
     my_posts.append(post_dict)
-    return f"Your post is successfully created: \n Title:{posts.title} \n Content: {posts.content} \n Published: {posts.published} \n Reviews: {posts.reviews}"
+    # return f"Your post is successfully created: \n Title:{posts.title} \n Content: {posts.content} \n Published: {posts.published} \n Reviews: {posts.reviews}"
+    return {"data: " : post_dict}
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p 
+    
+
+@app.get("/posts/{id}")
+def get_specific_post(id: int):
+    post = find_post(int(id))
+    return {"Posts Details: Here is your post: ": post}
+    
